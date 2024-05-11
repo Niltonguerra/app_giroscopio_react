ee9d9c9210db4c2f909045045ea95ab98391b084
Nilton Dionisio Guerra <78931445+Niltonguerra@users.noreply.github.com>
2024-05-11 00:35:35 +0000

    update

--- app/index.tsx
+++ app/index.tsx
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Alert } from 'react-native';
 import { Accelerometer } from 'expo-sensors';
 import * as Location from 'expo-location';
+
+
 export default function App() {
   const [data, setData] = useState({ x: 0, y: 0, z: 0 });
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
+
+  const [location, setLocation] = useState <Location.LocationObject>({coords:{latitude:0,longitude:0,accuracy:0,altitude:0,altitudeAccuracy:0,heading:0,speed:0,},timestamp:0});
+  const [errorMsg, setErrorMsg] = useState("null");
 
   useEffect(() => {
     subscribe();
@@ -35,7 +38,7 @@ export default function App() {
     Accelerometer.removeAllListeners();
   };
 
-  const checkForFall = async ({ x, y, z }) => {
+  const checkForFall = async ({ x, y, z }:{x:number,y:number,z:number}) => {
     const limit = 1.5; // Limite de aceleração para detecção de queda
     if (Math.abs(x) > limit || Math.abs(y) > limit || Math.abs(z) > limit) {
       let location = await Location.getCurrentPositionAsync({});
@@ -44,7 +47,7 @@ export default function App() {
     }
   };
 
-  const alertFall = (location) => {
+  const alertFall = (location:Location.LocationObject) => {
     Alert.alert(
       "Queda Detectada",
       `Uma queda foi detectada. Localização: ${location.coords.latitude}, ${location.coords.longitude}`,
@@ -56,6 +59,7 @@ export default function App() {
     <View style={styles.container}>
       <Text style={styles.text}>Detector de Quedas</Text>
       <Text style={styles.text}>{errorMsg ? errorMsg : `Latitude: ${location ? location.coords.latitude : ''}, Longitude: ${location ? location.coords.longitude : ''}`}</Text>
+      <Text  style={styles.text}>{ errorMsg: `X: ${data.x},Y: ${data.y},Z: ${data.z}`} </Text>
     </View>
   );
 }
